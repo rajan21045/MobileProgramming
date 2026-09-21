c7ce99265a8dd63765e9e8ab5f5aba70ae75a7e7
Rajan Poudel <raajan.works@gmail.com>
2026-09-21 20:31:24 +0545

    I have updated a file

--- First Application/app/src/main/java/com/example/application/RegistrationActivity.java
+++ First Application/app/src/main/java/com/example/application/RegistrationActivity.java
@@ -3,7 +3,9 @@ package com.example.application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,23 +16,81 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    Spinner spCity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_registration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        Button btnLogin = findViewById(R.id.btnRegistration);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_registration);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars = insets.getInsets(
+                            WindowInsetsCompat.Type.systemBars()
+                    );
+
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom
+                    );
+
+                    return insets;
+                }
+        );
+
+        // City Spinner
+        spCity = findViewById(R.id.spCity);
+
+        String[] cities = {
+                "Select City",
+                "Kathmandu",
+                "Pokhara",
+                "Bharatpur",
+                "Butwal",
+                "Lalitpur",
+                "Biratnagar",
+                "Birgunj"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                cities
+        );
+
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        spCity.setAdapter(adapter);
+
+
+        // Registration Button
+        Button btnRegistration = findViewById(R.id.btnRegistration);
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(RegistrationActivity.this, "Home Clicked", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(RegistrationActivity.this, HomeActivity.class);
+
+                String selectedCity = spCity.getSelectedItem().toString();
+
+                Toast.makeText(
+                        RegistrationActivity.this,
+                        "Selected City: " + selectedCity,
+                        Toast.LENGTH_SHORT
+                ).show();
+
+                Intent i = new Intent(
+                        RegistrationActivity.this,
+                        HomeActivity.class
+                );
+
                 startActivity(i);
             }
         });
